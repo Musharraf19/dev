c089d58bb8f4feebee73520f08e0e10b9a98daf8
Musharraf19 <125123797+Musharraf19@users.noreply.github.com>
2024-11-20 16:27:00 +0530

    Add files via upload

---
+++ first.java
@@ -0,0 +1,7 @@
+public class personalinfo{
+    public static void main(String[]args){
+        System.out.println("Name:Musharraf");
+        System.out.println("Age:21");
+        System.out.println("RegNO:169");
+    }
+}
